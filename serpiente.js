3e018619d9bd53b86d3dfd47097a00901bc8f718
MateoM2008 <mateomunozlema000@gmail.com>
2026-07-30 17:22:05 -0500

    agregar la cuadricula al canvas

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,44 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
 
 
-    
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
+
+function dibujarTablero() {
+
+    ctx.strokeStyle = "#555";
+
+    // Líneas verticales
+    for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
     }
 
-    function dibujarTodo() {
-      limpiarCanvas();
+    // Líneas horizontales
+    for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
+        ctx.stroke();
     }
-
-
-
+}
